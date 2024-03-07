b71dbc2efdf37f98a6e5fc17f70ceb6191b04242
jangsubin <eltm521@naver.com>
2024-03-07 18:55:44 +0900

    [Feat] : swagger setting

--- src/main/java/Fo/Suzip/SuzipApplication.java
+++ src/main/java/Fo/Suzip/SuzipApplication.java
@@ -2,8 +2,9 @@ package Fo.Suzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SuzipApplication {
 
 	public static void main(String[] args) {
