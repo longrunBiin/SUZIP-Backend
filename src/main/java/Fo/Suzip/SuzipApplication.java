ff0c93deaf7933154bec6e50996b93380c806faa
jangsubin <eltm521@naver.com>
2024-03-07 18:40:31 +0900

    [Feat] : SpringBoot & application.yml 초기세팅

---
+++ src/main/java/Fo/Suzip/SuzipApplication.java
@@ -0,0 +1,14 @@
+package Fo.Suzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuzipApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/Fo/Suzip/SuzipApplicationTests.java
@@ -0,0 +1,13 @@
+package Fo.Suzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
